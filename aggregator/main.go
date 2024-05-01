dc59b05bf6dbcf753f4dbd6b2f772e60be509176
Haowei Lee <haoweilee.tpe@gmail.com>
2024-05-01 11:08:15 +0800

    add grpc transport layer

--- aggregator/main.go
+++ aggregator/main.go
@@ -4,14 +4,17 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"strconv"
 
 	"github.com/leehaowei/tolling-micro-service/types"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	listenAddr := flag.String("listenaddr", ":3000", "the listen addres of the HTTP server")
+	httpListenAddr := flag.String("httpAddr", ":3000", "the listen addres of the HTTP server")
+	grpcListenAddr := flag.String("grpcAddr", ":3001", "the listen addres of the GRPC server")
 	flag.Parse()
 
 	var (
@@ -20,7 +23,23 @@ func main() {
 	)
 	svc = NewLogMiddleware(svc)
 
-	makeHTTPTransport(*listenAddr, svc)
+	go makeGRPCTransport(*grpcListenAddr, svc)
+	makeHTTPTransport(*httpListenAddr, svc)
+}
+
+func makeGRPCTransport(listenAddr string, svc Aggregator) error {
+	fmt.Println("GRPC transport running on port ", listenAddr)
+	// Make a TCP Listener
+	ln, err := net.Listen("TCP", listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	// Make a new GRPC native sever with (options)
+	server := grpc.NewServer([]grpc.ServerOption{}...)
+	// Register our GRPC sever implementation to the GRPC package
+	types.RegisterAggregatorServer(server, NewGRPCAggregatorServer(svc))
+	return server.Serve(ln)
 }
 
 func makeHTTPTransport(listenAddr string, svc Aggregator) {
